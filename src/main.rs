4f03f88338923a307f7b534a1a4063856ff2192e
RockRockWhite <1834309151@qq.com>
2023-05-05 22:32:54 +0800

    mermaid parse dfa

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ fn main() {
     // let postfix = rlex::to_postfix("c(a|bbcb*)*(ab)");
     let postfix = rlex::to_postfix("c(abc|b*)");
     let nfa = rlex::to_nfa(&postfix);
-    let mermaid = rlex::mermaid::parse_nfa(&nfa);
-    println!("{}", mermaid);
+    let dfa = rlex::to_dfa(&nfa);
 
-    rlex::to_dfa(&nfa);
+    let mermaid = rlex::mermaid::parse_nfa(&nfa);
+    let mermaid = rlex::mermaid::parse_dfa(&dfa);
+    println!("{}", mermaid);
 }
--- src/mermaid.rs
+++ src/mermaid.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use crate::{nfa::StateVertex, NFA};
+use crate::{nfa::StateVertex, DfaVertexRef, NFA};
 
 /// Converts an NFA to a mermaid graph
 /// https://mermaid-js.github.io/mermaid/#/graph?id=graph
@@ -99,6 +99,72 @@ fn tarverse_nfa_vertex(
 
             // 添加当前节点到该节点的边
             edges.push_str(&format!("{}--{}-->{}\n", id, "ε", neighbor_id));
+        });
+
+    edges
+}
+
+/// Converts an DFA to a mermaid graph
+/// https://mermaid-js.github.io/mermaid/#/graph?id=graph
+/// ```mermaid
+/// graph LR
+/// A((A))
+/// B((B))
+/// A--a-->B
+/// ```
+pub fn parse_dfa(vertex: &DfaVertexRef) -> String {
+    // 遍历图
+    let mut visited = Vec::new();
+    let edge = tarverse_dfa_vertex(DfaVertexRef::clone(&vertex), &mut visited);
+
+    // 添加节点
+    let mut vertex = String::new();
+    visited.iter().enumerate().for_each(|(id, each)| {
+        vertex.push_str(&format!("{}(({}))\n", id, id));
+    });
+
+    format!("graph LR\n{}\n{}", vertex, edge)
+}
+
+/// tarverse_dfa_vertex
+/// 遍历dfa节点，返回其边
+/// return edges
+fn tarverse_dfa_vertex(vertex: DfaVertexRef, visited: &mut Vec<DfaVertexRef>) -> String {
+    let mut edges = String::new();
+
+    // 如果已经访问过，直接返回
+    if visited.contains(&vertex) {
+        return edges;
+    }
+
+    // 标记为已访问, 将vec的下标作为id
+    let id = visited.len();
+    visited.push(DfaVertexRef::clone(&vertex));
+
+    let find_vertex_id = |vertex: DfaVertexRef, visited: &Vec<DfaVertexRef>| -> usize {
+        visited
+            .iter()
+            .enumerate()
+            .find(|(_, each)| (**each) == vertex)
+            .unwrap()
+            .0
+    };
+
+    // 遍历节点
+
+    vertex
+        .borrow()
+        .neighbors
+        .iter()
+        .for_each(|(&cond, vertex)| {
+            let neighbor_edges = tarverse_dfa_vertex(DfaVertexRef::clone(&vertex), visited);
+            // 将结果添加到边中
+            edges.push_str(&neighbor_edges);
+
+            let neighbor_id = find_vertex_id(DfaVertexRef::clone(&vertex), visited);
+
+            // 添加当前节点到该节点的边
+            edges.push_str(&format!("{}--{}-->{}\n", id, cond as char, neighbor_id));
         });
 
     edges
